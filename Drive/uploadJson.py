1709ab39571c9e6b19e08d8a664779cb743ff24e
陳立恒 <leonchen@MacBook-Pro.local>
2019-04-24 15:34:52 -0500

    driveapi

--- Drive/uploadJson.py
+++ Drive/uploadJson.py
@@ -2,16 +2,20 @@ import sys
 from pydrive.drive import GoogleDrive
 from pydrive.auth import GoogleAuth
 import os
+import time
 # os.system('')
 
-gauth = GoogleAuth()
-# Creates local webserver and auto handles authentication.
-gauth.LocalWebserverAuth()
-drive = GoogleDrive(gauth)
-try:
-    name = 'README.md'  # It's the file which you'll upload
-    file = drive.CreateFile()  # Create GoogleDriveFile instance
-    file.SetContentFile(name)
-    file.Upload()
-except:
-    print("Unexpected error:", sys.exc_info()[0])
+while True:
+    gauth = GoogleAuth()
+    # Creates local webserver and auto handles authentication.
+    gauth.LocalWebserverAuth()
+    drive = GoogleDrive(gauth)
+    try:
+        name = 'README.md'  # It's the file which you'll upload
+        file = drive.CreateFile()  # Create GoogleDriveFile instance
+        file.SetContentFile(name)
+        file.Upload()
+    except:
+        print("Unexpected error:", sys.exc_info()[0])
+    
+    time.sleep(30)
